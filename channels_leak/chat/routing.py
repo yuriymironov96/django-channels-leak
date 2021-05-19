9b4489f07934c5b43a1e8685524ab0c2d7c32245
Yurii Mironov <yuriymironov96@gmail.com>
2021-05-19 10:58:18 +0300

    Comment out debug statements

--- channels_leak/chat/routing.py
+++ channels_leak/chat/routing.py
@@ -4,7 +4,7 @@ from . import consumers
 
 import tracemalloc
 
-tracemalloc.start()
+# tracemalloc.start()
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
